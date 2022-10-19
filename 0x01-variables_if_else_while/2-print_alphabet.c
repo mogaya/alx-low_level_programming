790315f4072e340038a56ea689789863b4e97008
Mogaya <mogayainnocent@gmail.com>
2022-10-19 09:42:24 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * 2-print_alphabet.c
+ * Mogaya
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
