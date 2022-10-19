261a26712d9681b70687e3ab6953003bf531ec3b
Mogaya <mogayainnocent@gmail.com>
2022-10-19 14:21:32 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
-#include "stdio.h"
+#include "main.h"
+
 /**
- * main - Entry point
- *
+ * main - prints _putchar
  * Return: Always 0
  */
 int main(void)
 {
-	char word[] = "_putchar";
-
-	printf("%s", word);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
