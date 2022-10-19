cdbde4ab688e5a7639a949ef6c58112494a8480b
Mogaya <mogayainnocent@gmail.com>
2022-10-19 10:43:20 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * 5-print_numbers.c
+ * Mogaya
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		printf("%d", number);
+
+	printf("\n");
+
+	return (0);
+}
