50517913cade943206ad146dd2d43b996362bec9
Mogaya <mogayainnocent@gmail.com>
2022-10-25 16:44:54 -0700

    Fix 3

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,20 +3,21 @@
 /**
  * _strcpy - Copies a string pointed to by @src, including the
  *           terminating null byte, to a buffer pointed to by @dest.
- * @dest: A buffer to copy the string to.
+ * @dest: destination.
  * @src: The source string to copy.
  *
  * Return: A pointer to the destination string @dest.
  */
 char *_strcpy(char *dest, const char *src)
 {
-	int index = 0;
+	int len = 0;
 
-	while (src[index])
+	while (*(src + len) != '\0')
 	{
-		dest[index] = src[index];
-		index++;
+		*(dest + len) = *(src + len);
+		len++;
 	}
 
+	*(dest + len) = '\0';
 	return (dest);
 }
